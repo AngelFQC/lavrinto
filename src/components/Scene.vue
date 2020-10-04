79f69a9f750ae46a271b19f7de5ace2bf40990d8
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-10-04 01:34:29 -0500

    Add aframe

---
+++ src/components/Scene.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-scene>
+    <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#43C877"></a-plane>
+    <a-sky color="#FFF"></a-sky>
+  </a-scene>
+</template>
+
+<script>
+import 'aframe'
+
+export default {
+  name: 'Scene'
+}
+</script>
+
+<style>
+</style>
